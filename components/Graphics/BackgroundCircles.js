4a9a6ee4c583728d1b150fd184dfbd0525096293
Grubauer <44618705+Grubauer@users.noreply.github.com>
2022-01-12 21:48:05 +0100

    bugfixes

--- components/Graphics/BackgroundCircles.js
+++ components/Graphics/BackgroundCircles.js
@@ -32,7 +32,6 @@ export default function BackgroundCircles()
             }
             else if(counter < keys.length)
             {
-                console.log(keys[counter]);
                 const circle = circles.current[keys[counter]];
                 if(circle)
                 {
@@ -79,7 +78,7 @@ export default function BackgroundCircles()
                 cx={630}
                 cy={569}
                 r={circleProps.startRadius + (i + 1) * circleProps.radiusGrowStep}
-                stroke-width="10"
+                strokeWidth="10"
                 ref={el => circles.current[circleProps.startRadius + circleProps.radiusGrowStep * (i+1)] = el}
                 stroke={`url(#paint${i+1}_linear_300_55)`}/>
         )}
@@ -92,36 +91,36 @@ export default function BackgroundCircles()
         {/*<circle opacity="0.1" cx="630" cy="569" r="625" stroke="url(#paint7_linear_300_55)" stroke-width="10"/>*/}
         <defs>
             <linearGradient id="paint0_linear_300_55" x1="510.796" y1="561.099" x2="1038.45" y2="451.386" gradientUnits="userSpaceOnUse">
-                <stop stop-color="#DA22FF"/>
-                <stop offset="1" stop-color="#9733EE"/>
+                <stop stopColor="#DA22FF"/>
+                <stop offset="1" stopColor="#9733EE"/>
             </linearGradient>
             <linearGradient id="paint1_linear_300_55" x1="489.122" y1="559.663" x2="1112.71" y2="430.001" gradientUnits="userSpaceOnUse">
-                <stop stop-color="#DA22FF"/>
-                <stop offset="1" stop-color="#9733EE"/>
+                <stop stopColor="#DA22FF"/>
+                <stop offset="1" stopColor="#9733EE"/>
             </linearGradient>
             <linearGradient id="paint2_linear_300_55" x1="467.449" y1="558.227" x2="1186.97" y2="408.617" gradientUnits="userSpaceOnUse">
-                <stop stop-color="#DA22FF"/>
-                <stop offset="1" stop-color="#9733EE"/>
+                <stop stopColor="#DA22FF"/>
+                <stop offset="1" stopColor="#9733EE"/>
             </linearGradient>
             <linearGradient id="paint3_linear_300_55" x1="445.775" y1="556.79" x2="1261.23" y2="387.232" gradientUnits="userSpaceOnUse">
-                <stop stop-color="#DA22FF"/>
-                <stop offset="1" stop-color="#9733EE"/>
+                <stop stopColor="#DA22FF"/>
+                <stop offset="1" stopColor="#9733EE"/>
             </linearGradient>
             <linearGradient id="paint4_linear_300_55" x1="424.102" y1="555.354" x2="1335.5" y2="365.848" gradientUnits="userSpaceOnUse">
-                <stop stop-color="#DA22FF"/>
-                <stop offset="1" stop-color="#9733EE"/>
+                <stop stopColor="#DA22FF"/>
+                <stop offset="1" stopColor="#9733EE"/>
             </linearGradient>
             <linearGradient id="paint5_linear_300_55" x1="402.428" y1="553.917" x2="1409.76" y2="344.463" gradientUnits="userSpaceOnUse">
-                <stop stop-color="#DA22FF"/>
-                <stop offset="1" stop-color="#9733EE"/>
+                <stop stopColor="#DA22FF"/>
+                <stop offset="1" stopColor="#9733EE"/>
             </linearGradient>
             <linearGradient id="paint6_linear_300_55" x1="380.754" y1="552.481" x2="1484.02" y2="323.079" gradientUnits="userSpaceOnUse">
-                <stop stop-color="#DA22FF"/>
-                <stop offset="1" stop-color="#9733EE"/>
+                <stop stopColor="#DA22FF"/>
+                <stop offset="1" stopColor="#9733EE"/>
             </linearGradient>
             <linearGradient id="paint7_linear_300_55" x1="359.081" y1="551.044" x2="1558.28" y2="301.694" gradientUnits="userSpaceOnUse">
-                <stop stop-color="#DA22FF"/>
-                <stop offset="1" stop-color="#9733EE"/>
+                <stop stopColor="#DA22FF"/>
+                <stop offset="1" stopColor="#9733EE"/>
             </linearGradient>
         </defs>
     </Svg>
