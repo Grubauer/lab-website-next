5bcaa9e06f0b77a333ee0480a46ebe9e54e55fa0
Grubauer <44618705+Grubauer@users.noreply.github.com>
2022-01-17 16:08:39 +0100

    transition changes

--- components/Objects/AnimatedPhoneProject.js
+++ components/Objects/AnimatedPhoneProject.js
@@ -28,6 +28,18 @@ const TextOverlay = styled.div`
     z-index: 1;
     transform: translate(-50%, -50%);
     pointer-events: none;
+    
+   
+    > *{
+        transition: all 0.5s ease-in-out;
+        opacity: 0;
+        transform: translateY(100%);
+        ${props => props.hovered && `
+            opacity: 1;
+            transform: translateY(0px);
+            transition: all 0.5s ease-in-out 0.2s;
+        `}
+    }
    
 `
 
@@ -35,27 +47,17 @@ const ProjectTitle = styled.h3`
     font-size: 2rem;
     font-weight: 500;
     color: #fff;
-    position: absolute;
+   
     height: 50px;
-    overflow: hidden;
-    top: -50%;
-    left: 50%;
-    margin: 0;
     opacity: 0;
-    transform: translate(-50%, -50%);
-    color: ${props => props.theme.colors.primary};
+    margin: 0;
+   
+    
     
 `
 
-const ProjectTitleWrapper = styled.div`
-        position: relative;
-        overflow: hidden;
-        height: 40px;
-        width: 100%;
-        pointer-events: none;
-    `
 const ProjectDescription = styled.p`
-    opacity: 0;
+  
     color: #fff;
     max-width: 300px;
     font-size: 14px;
@@ -95,17 +97,16 @@ export function AnimatedPhoneProject({project, img_count, onPhoneHovered, canvas
         }
         if(hovered)
         {
-            titleAnimation = gsap.to("#projectTitle" + canvasId, {duration: 1, color: "#fff", top: "50%", opacity: 1, ease: "power3.out", delay: 0.6});
-            descriptionAnimation = gsap.to("#projectDescription" + canvasId, {duration: 1, top: "50%", opacity: 1, ease: "power3.out", delay: 1});
+            // titleAnimation = gsap.to("#projectTitle" + canvasId, {duration: 1, color: "#fff", top: "50%", opacity: 1, ease: "power3.out", delay: 0.6});
+            // descriptionAnimation = gsap.to("#projectDescription" + canvasId, {duration: 1, top: "50%", opacity: 1, ease: "power3.out", delay: 1});
             // gsap.to("#projectHint", {duration: 1, top: "50%", opacity: 1, ease: "power3.out", delay: 1.4});
 
         }
-        else{
-
-            titleAnimation = gsap.to("#projectTitle" + canvasId, {duration: 1, top: "-50%", opacity: 0, ease: "power3.out", delay: 0});
-            descriptionAnimation = gsap.to("#projectDescription" + canvasId, {duration: 1, opacity: 0, ease: "power3.out", delay: 0});
-            // gsap.to("#projectHint", {duration: 1, top: "50%", opacity: 0, ease: "power3.out", delay: 0});
-        }
+        // else{
+        //     titleAnimation = gsap.to("#projectTitle" + canvasId, {duration: 1, top: "-50%", opacity: 0, ease: "power3.out", delay: 0});
+        //     descriptionAnimation = gsap.to("#projectDescription" + canvasId, {duration: 1, opacity: 0, ease: "power3.out", delay: 0});
+        //     // gsap.to("#projectHint", {duration: 1, top: "50%", opacity: 0, ease: "power3.out", delay: 0});
+        // }
     }, [hovered])
 
     const projects = {
@@ -135,12 +136,23 @@ export function AnimatedPhoneProject({project, img_count, onPhoneHovered, canvas
     const randomProjectIndex = Math.floor(Math.random() * Object.keys(projects).length);
     const activeProject = projects[project ? project : Object.keys(projects)[randomProjectIndex]];
 
+    let hoverTimoutRunning = false;
+    const setHoveredWithTimeOut = (hovered) => {
+       if(!hoverTimoutRunning)
+       {
+           setTimeout(() => {
+               setHovered(hovered);
+               hoverTimoutRunning = false;
+           }, 5000);
+       }
+    }
+
     return <AnimationContainer >
-        <PhoneAnimation onTouchStart={() => setHovered(true)} onTouchCanceled={() => setHovered(false)} onMouseEnter={() => setHovered(true)} onMouseLeave={() => setHovered(false)} canvasId={canvasId} activeProject={activeProject} />
-        <TextOverlay style={{display: hovered ? "flex" : "none"}}>
-            <ProjectTitleWrapper>
-                <ProjectTitle id={"projectTitle" + canvasId}>{activeProject.title}</ProjectTitle>
-            </ProjectTitleWrapper>
+        <PhoneAnimation onTouchStart={() => setHovered(true)} onTouchEnd={() => setHoveredWithTimeOut(false)} onMouseEnter={() => setHovered(true)} onMouseLeave={() => setHovered(false)} canvasId={canvasId} activeProject={activeProject} />
+        <TextOverlay hovered={hovered}>
+
+            <ProjectTitle id={"projectTitle" + canvasId}>{activeProject.title}</ProjectTitle>
+
             <ProjectDescription id={"projectDescription" + canvasId}>
                 {activeProject.description}
             </ProjectDescription>
