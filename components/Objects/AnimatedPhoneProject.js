95771d7861542a44a1f8b3f440e7c8c1431aa25a
Grubauer <44618705+Grubauer@users.noreply.github.com>
2022-01-17 15:35:21 +0100

    onTouch

--- components/Objects/AnimatedPhoneProject.js
+++ components/Objects/AnimatedPhoneProject.js
@@ -136,7 +136,7 @@ export function AnimatedPhoneProject({project, img_count, onPhoneHovered, canvas
     const activeProject = projects[project ? project : Object.keys(projects)[randomProjectIndex]];
 
     return <AnimationContainer >
-        <PhoneAnimation onMouseEnter={() => setHovered(true)} onMouseLeave={() => setHovered(false)} canvasId={canvasId} activeProject={activeProject} />
+        <PhoneAnimation onTouchStart={() => setHovered(true)} onTouchEnd={() => setHovered(false)} onMouseEnter={() => setHovered(true)} onMouseLeave={() => setHovered(false)} canvasId={canvasId} activeProject={activeProject} />
         <TextOverlay style={{display: hovered ? "flex" : "none"}}>
             <ProjectTitleWrapper>
                 <ProjectTitle id={"projectTitle" + canvasId}>{activeProject.title}</ProjectTitle>
