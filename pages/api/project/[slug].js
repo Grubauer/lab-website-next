5906db7063070b6e854bd53d69ea6f47bedefab7
Grubauer <44618705+Grubauer@users.noreply.github.com>
2022-02-02 13:18:57 +0100

    Update [slug].js

--- pages/api/project/[slug].js
+++ pages/api/project/[slug].js
@@ -1,11 +1,11 @@
-import {projects} from "../projects";
-
-export default function handler(req, res) {
-    const {
-        query: { slug },
-        method,
-    } = req
-    console.log(`${method} request for ${slug}`, projects.find(project => project.attributes.slug === slug));
-
-    res.status(200).json(projects.find(project => project.attributes.slug === slug));
-}
+// import {projects} from "../projects";
+//
+// export default function handler(req, res) {
+//     const {
+//         query: { slug },
+//         method,
+//     } = req
+//     console.log(`${method} request for ${slug}`, projects.find(project => project.attributes.slug === slug));
+//
+//     res.status(200).json(projects.find(project => project.attributes.slug === slug));
+// }
